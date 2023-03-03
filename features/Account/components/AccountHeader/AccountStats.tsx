50c4e164321608b684b991d8e696230384b32806
0xcappy <0xcappy@protonmail.com>
2023-03-03 22:43:39 +1100

    clean up

--- features/Account/components/AccountHeader/AccountStats.tsx
+++ features/Account/components/AccountHeader/AccountStats.tsx
@@ -1,24 +1,6 @@
 import { Account } from "@/src/API"
 import { Box, Card, createStyles, SimpleGrid, Text } from '@mantine/core';
 
-const data = [
-    {
-        "title": "Page views",
-        "stats": "456,133",
-        "description": "24% more than in the same month last year, 33% more that two years ago"
-    },
-    {
-        "title": "New users",
-        "stats": "2,175",
-        "description": "13% less compared to last month, new user engagement up by 6%"
-    },
-    {
-        "title": "Completed orders",
-        "stats": "1,994",
-        "description": "1994 orders were completed this month, 97% satisfaction rate"
-    }
-]
-
 const useStyles = createStyles((theme) => ({
     root: {
         display: 'flex',
