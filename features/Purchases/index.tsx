2a274ce993a2e4243d1e082c52c81257a98461ee
0xcappy <0xcappy@protonmail.com>
2023-03-04 02:36:14 +1100

    change

--- features/Purchases/index.tsx
+++ features/Purchases/index.tsx
@@ -11,6 +11,7 @@ import { useEffect, useMemo, useState } from "react";
 import { Table, Column, HeaderCell, Cell, SortType } from 'rsuite-table';
 import 'rsuite-table/dist/css/rsuite-table.css'
 
+
 const PAGE_LENGTH = 20
 
 interface Filter {
