665de9e85c16019270215f404df449d27396d1c6
0xcappy <0xcappy@protonmail.com>
2023-03-04 02:54:19 +1100

    fix ts error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -75,7 +75,7 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
         <meta name="description" content="DeRafl brings you the opportunity to win unique digital assets with our revolutionary decentrailized raffle protocol" />
         <link rel="shortcut icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300;400;500;600&display=swap" rel="stylesheet" />      
         <script type="text/javascript" async src="//platform.twitter.com/widgets.js"></script>
         <meta property="og:title" content="DeRafl - Decentralized NFT Raffles"/>
