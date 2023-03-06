c6f1c7880fc500e5cd53c9d2188eaa988a3af57e
0xcappy <0xcappy@protonmail.com>
2023-03-07 00:35:01 +1100

    fix winning batch query

--- features/RaffleDetail/index.tsx
+++ features/RaffleDetail/index.tsx
@@ -90,14 +90,13 @@ const RaffleDetail = ({ raffle, trending }: RaffleDetailProps) => {
     }, [raffleInfo])
 
     const progress: number = useMemo(() => {
-        if (raffleInfo) {
-            const sold = raffleInfo.ticketsSold.toNumber()
-            const available = raffleInfo.ticketsAvailable.toNumber()
-
+        if (_raffleInfo) {
+            const sold = _raffleInfo.ticketsSold.toNumber()
+            const available = _raffleInfo.ticketsAvailable.toNumber()
             return (sold * 100) / available
         }
         return 0
-    }, [raffleInfo])
+    }, [_raffleInfo])
 
     const renderRaffleAction = () => (
         <>
