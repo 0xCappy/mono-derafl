fe1d4cc00d1c23d09ad4ab7f86f78c307ec7084e
0xcappy <0xcappy@protonmail.com>
2023-06-01 10:47:00 +1000

    cleanup js from streamevent function

--- amplify/backend/function/deraflstreamevent/src/clients/OpenseaClient.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// import config from "../../config"
-// import axios from 'axios';
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=OpenseaClient.js.map
